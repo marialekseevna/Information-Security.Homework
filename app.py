d83acbf2e761794a28367decc70b17490267851f
unknown <marykut99@gmail.com>
2020-10-07 22:39:22 +0400

    Cipher Vizhenera

---
+++ app.py
@@ -0,0 +1,16 @@
+from main import Cipher
+
+cip = Cipher()
+
+key = input('Введите ключ: ')
+text = input('Введите слово для шифрования: ')
+key_text = input('Введите слово для расшифрования: ')
+
+d = cip.cipher(key, text, key_text)
+
+
+print(d)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+alf = 'abcdefghijklmnopqrstuvwxyz'
+
+class Cipher:
+    # шифруем
+    def encrypt(self, key, text):
+        result = []
+        space = 0
+        for index, ch in enumerate(text):
+            if ch != ' ':
+                mj = alf.index(ch)
+                kj = alf.index(key[(index - space) % len(key)])
+                cj = (mj + kj) % len(alf)
+                result.append(alf[cj])
+            else:
+                space += 1
+                result.append(' ')
+        return ''.join(result)
+    # расшифровываем
+    def decrypt(self, key, key_text):
+        result = []
+        space = 0
+        for index, ch in enumerate(key_text):
+            if ch != ' ':
+                cj = alf.index(ch)
+                kj = alf.index(key[(index - space) % len(key)])
+                mj = (cj - kj) % len(alf)
+                result.append(alf[mj])
+            else:
+                space += 1
+                result.append(' ')
+        return ''.join(result)
+
+
+    def cipher(self, key, text=None, key_text=None):
+        if not key_text:
+            return self.encrypt(key, text)
+        else:
+            return self.decrypt(key, key_text)
